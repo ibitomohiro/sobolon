cdfb677e6648d0d241177d872b64b82d73eb6b42
tomohiro ibi <68018348+ibitomohiro@users.noreply.github.com>
2021-07-25 21:23:52 +0900

    送信の処理を実装

--- js/script.js
+++ js/script.js
@@ -21,5 +21,29 @@ $(function(){
   });
   // wowjs
   new WOW().init()
-});
 
+  // google_form
+  let $form = $('#js-form')
+$form.submit(function(e) {
+  $.ajax({
+   url: $form.attr('action'),
+   data: $form.serialize(),
+   type: "POST",
+   dataType: "xml",
+   statusCode: {
+      0: function() {
+        //送信に成功したときの処理
+        $form.slideUp()
+        $('#js-success').slideDown()
+      },
+      200: function() {
+        //送信に失敗したときの処理
+        $form.slideUp()
+        $('#js-error').slideDown()
+      }
+    }
+  });
+
+  return false;
+  });
+})
